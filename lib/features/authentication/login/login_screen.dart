01b38b76adaccc8c7249db3e2db2af930a7194fe
kassem <kassemdiaa537@gmail.com>
2025-12-08 00:12:25 +0200

    social media urls

--- lib/features/authentication/login/login_screen.dart
+++ lib/features/authentication/login/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tazkarti/core/colors/colors.dart';
 import 'package:tazkarti/core/fonts/fonts.dart';
 import 'package:tazkarti/features/sidebar/drawer.dart';
@@ -19,11 +20,16 @@ class _LoginScreenState extends State<LoginScreen> {
       key: _scaffoldKey,
       appBar: AppBar(
         backgroundColor: ColorsManeger.black,
+        title: Text('TAZKARTI',style: TextStyle(color: ColorsManeger.green,fontWeight: FontWeight.w700),),
+        actions: [Padding(
+          padding:  EdgeInsets.symmetric(horizontal: 8.w),
+          child: Icon(CupertinoIcons.globe,color: ColorsManeger.white,size: 28,),
+        )],
         leading: IconButton(
           onPressed: () {
             _scaffoldKey.currentState!.openDrawer();
           },
-          icon: Icon(CupertinoIcons.line_horizontal_3,color: ColorsManeger.white,),
+          icon: Icon(CupertinoIcons.line_horizontal_3,color: ColorsManeger.white,size: 26),
         ),
       ),
       drawer: CustomDrawer(),
--- lib/features/sidebar/drawer.dart
+++ lib/features/sidebar/drawer.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tazkarti/core/colors/colors.dart';
 import 'package:tazkarti/core/fonts/fonts.dart';
 import 'package:tazkarti/core/routes/routes.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class CustomDrawer extends StatefulWidget {
   const CustomDrawer({super.key});
@@ -70,29 +72,63 @@ class _CustomDrawerState extends State<CustomDrawer> {
               SizedBox(height: 10),
               Container(color: ColorsManeger.white, width: 270, height: 1),
               SizedBox(height: 10),
-              InkWell(onTap: () {
-                Navigator.pushNamed(context, RoutesManeger.faqScreen);
-              },child: Text('FAQ', style: FontManeger.eventdetails)),
+              InkWell(
+                onTap: () {
+                  Navigator.pushNamed(context, RoutesManeger.faqScreen);
+                },
+                child: Text('FAQ', style: FontManeger.eventdetails),
+              ),
               SizedBox(height: 10),
               Container(color: ColorsManeger.white, width: 270, height: 1),
               SizedBox(height: 10),
-              InkWell(onTap: () {
-                Navigator.pushNamed(context, RoutesManeger.aboutScreen);
-              },child: Text('ABOUT TAZKARTI', style: FontManeger.eventdetails)),
+              InkWell(
+                onTap: () {
+                  Navigator.pushNamed(context, RoutesManeger.aboutScreen);
+                },
+                child: Text('ABOUT TAZKARTI', style: FontManeger.eventdetails),
+              ),
               SizedBox(height: 10),
               Container(color: ColorsManeger.white, width: 270, height: 1),
               SizedBox(height: 10),
-              InkWell(onTap: () {
-                Navigator.pushNamed(context, RoutesManeger.contactUsScreen);
-              },child: Text('CONTACT US', style: FontManeger.eventdetails)),
+              InkWell(
+                onTap: () {
+                  Navigator.pushNamed(context, RoutesManeger.contactUsScreen);
+                },
+                child: Text('CONTACT US', style: FontManeger.eventdetails),
+              ),
             ],
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              Icon(Icons.facebook, color: ColorsManeger.gray),
-              Icon(Icons.facebook, color: ColorsManeger.gray),
-              Icon(Icons.facebook, color: ColorsManeger.gray),
+              IconButton(
+                onPressed: () async {
+                  final url = Uri.parse("https://www.facebook.com/tazkarti");
+
+                  await launchUrl(url, mode: LaunchMode.inAppBrowserView);
+                },
+                icon: Icon(Icons.facebook, color: ColorsManeger.gray,size: 30,),
+              ),
+              IconButton(
+                onPressed: () async {
+                  final url = Uri.parse("https://x.com/Tazkarti");
+
+                  await launchUrl(url, mode: LaunchMode.externalApplication);
+                },
+                icon: Icon(FontAwesomeIcons.twitter, color: ColorsManeger.gray,size: 30,),
+              ),
+              IconButton(
+                onPressed: () async {
+                  final url = Uri.parse("https://www.instagram.com/tazkarti");
+
+                  await launchUrl(url, mode: LaunchMode.externalApplication);
+                },
+                icon: Icon(
+                  FontAwesomeIcons.instagram,
+                  color: ColorsManeger.gray,
+                  size: 30,
+                ),
+              ),
             ],
           ),
         ],
