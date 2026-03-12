885c2e35f8bc6530683af5795c1aff6cf2c0df07
Kassem Diaa <kassemdiaa537@gmail.com>
2026-03-12 06:12:25 +0200

    finish logic of events screen

--- lib/features/mainlayout/events/events/event_item.dart
+++ lib/features/mainlayout/events/events/event_item.dart
@@ -3,68 +3,73 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tazkarti/core/colors/colors.dart';
 import 'package:tazkarti/core/fonts/fonts.dart';
 
-class EventItem extends StatefulWidget {
+class EventItem extends StatelessWidget {
   const EventItem({
     super.key,
     required this.picPath,
     required this.title,
     required this.numOfShows,
     required this.location,
-    required this.date,
+    required this.day,
+    required this.month,
+    required this.year,
     required this.price,
   });
   final String picPath;
   final String title;
   final String numOfShows;
   final String location;
-  final String date;
+  final String day;
+  final String month;
+  final String year;
   final String price;
 
   @override
-  State<EventItem> createState() => _EventItemState();
-}
-
-class _EventItemState extends State<EventItem> {
-  @override
   Widget build(BuildContext context) {
     return Padding(
-      padding:  EdgeInsets.symmetric(horizontal: 8.w,vertical: 8.h),
+      padding: EdgeInsets.symmetric(horizontal: 8.w, vertical: 8.h),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Image.asset(widget.picPath),
-          SizedBox(height: 6.h,),
-          Text(widget.title, style: FontManeger.name),
-          SizedBox(height: 6.h,),
+          SizedBox(
+            width: double.infinity,
+            height: 160.h,
+            child: Image.asset(picPath,fit: BoxFit.fill,),
+          ),
+          SizedBox(height: 6.h),
+          Text(title, style: FontManeger.name),
+          SizedBox(height: 6.h),
           Row(
             children: [
               Text('No. of shows: ', style: FontManeger.eventDetails),
-              Text(widget.numOfShows, style: FontManeger.eventDetails),
+              Text(numOfShows, style: FontManeger.eventDetails),
             ],
           ),
-          SizedBox(height: 6.h,),
+          SizedBox(height: 6.h),
           Row(
             children: [
               Icon(Icons.location_on, color: ColorsManeger.red),
-              Text(widget.location, style: FontManeger.eventlocation),
+              Text(location, style: FontManeger.eventlocation),
             ],
           ),
-          SizedBox(height: 6.h,),
-          Text(widget.date, style: FontManeger.eventDetails),
-          SizedBox(height: 6.h,),
+          SizedBox(height: 6.h),
+          Text('$day $month $year', style: FontManeger.eventDetails),
+          SizedBox(height: 6.h),
           Row(
             children: [
               Text('Prices From: ', style: FontManeger.eventDetails),
-              Text(widget.price, style: FontManeger.eventDetails),
+              Text(price, style: FontManeger.eventDetails),
               Text(' EGP', style: FontManeger.eventDetails),
             ],
           ),
-          SizedBox(height: 6.h,),
+          SizedBox(height: 6.h),
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
-            children: [Text('Buy Tickets', style: FontManeger.eventBuy),SizedBox(width: 10.w,)],
+            children: [
+              Text('Buy Tickets', style: FontManeger.eventBuy),
+              SizedBox(width: 10.w),
+            ],
           ),
-          
         ],
       ),
     );
--- lib/features/mainlayout/events/events/events_screen.dart
+++ lib/features/mainlayout/events/events/events_screen.dart
@@ -2,12 +2,17 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:provider/provider.dart';
 import 'package:tazkarti/core/colors/colors.dart';
 import 'package:tazkarti/core/fonts/fonts.dart';
 import 'package:tazkarti/core/icons/Icons_maneger.dart';
+import 'package:tazkarti/core/routes/routes.dart';
 import 'package:tazkarti/core/widgets/header_info/header_info.dart';
 import 'package:tazkarti/core/widgets/search_button/search_button.dart';
+import 'package:tazkarti/features/mainlayout/events/events/event_item.dart';
 import 'package:tazkarti/features/sidebar/drawer.dart';
+import 'package:tazkarti/models/events_data/event_category_model.dart';
+import 'package:tazkarti/providers/selected_even_provider.dart/selected_event_provider.dart';
 
 class EventsScreen extends StatefulWidget {
   const EventsScreen({super.key});
@@ -20,6 +25,9 @@ class _CategoryScreenState extends State<EventsScreen> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
+    EventCategoryModel category = context
+        .read<SelectedEventProvider>()
+        .category;
     return Scaffold(
       backgroundColor: ColorsManeger.white,
       key: _scaffoldKey,
@@ -53,13 +61,39 @@ class _CategoryScreenState extends State<EventsScreen> {
           HeaderInfo(),
           SizedBox(height: 20.h),
           Padding(
-            padding:  EdgeInsets.symmetric(horizontal: 20.w),
-            child: Row(children: [
-              Text('Events', style: FontManeger.bigTitle),
-              Spacer(),
-              SearchButton()
-            ],),
-          )
+            padding: EdgeInsets.symmetric(horizontal: 20.w),
+            child: Row(
+              children: [
+                Text('Events', style: FontManeger.bigTitle),
+                Spacer(),
+                SearchButton(),
+              ],
+            ),
+          ),
+          SizedBox(height: 20.h),
+          Expanded(
+            child: ListView.separated(
+              itemBuilder: (context, index) => InkWell(
+                onTap: () {
+                  context.read<SelectedEventProvider>().selectEvent(category.events[index]);
+                  Navigator.pushNamed(context, RoutesManeger.eventsDeitailsScreen);
+                  
+                },
+                child: EventItem(
+                  picPath: category.events[index].imagePath,
+                  title: category.events[index].name,
+                  numOfShows: category.events[index].numOfShows,
+                  location: category.events[index].location,
+                  day:category.events[index].day ,
+                  month: category.events[index].month,
+                  year: category.events[index].year,
+                  price: category.events[index].categoriesPrices['Regular'].toString(),
+                ),
+              ),
+              separatorBuilder: (context, index) => SizedBox(height: 14.h),
+              itemCount: category.events.length,
+            ),
+          ),
         ],
       ),
     );
